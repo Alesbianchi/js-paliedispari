846373146a94a188fd62e65b82ea42264af56ea2
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-01-08 14:13:51 +0100

    complete palindrome

--- script/script.js
+++ script/script.js
@@ -5,4 +5,21 @@
 let parola = prompt('Inserisci una parola');
 console.log(parolaPalindroma(parola));
 
+function parolaPalindroma(parola) {
+    // variabile parola palindroma
+    let parolaInversa = '';
 
+    // creo un ciclo affinchè la parola venga letta al contrario
+    for (let i = parola.length - 1; i >= 0; i--) {
+        parolaInversa += parola[i];
+    }
+
+    // se la parola è palindroma restituisco un messaggio positivo
+    if (parola == parolaInversa) {
+        return 'La parola è palindroma';
+    
+    // altrimenti restituisco un messaggio negativo
+    } else {
+        return 'La parola non è palindroma';
+    }
+}
